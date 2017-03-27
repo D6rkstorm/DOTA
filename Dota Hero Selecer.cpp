4b967337503c27cd1d8259fa9633fe14b6f8946f
D6rkstorm <spqr.dominus@gmail.com>
2017-03-27 10:39:44 -0500

    first commi

---
+++ Dota Hero Selecer.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+struct Hero{
+	string name;
+	string role;
+	string additonalRole;
+};
+
+int main
+{
+	iosteam infile;
+	infile.open("Hero.csv");
+	
+}
